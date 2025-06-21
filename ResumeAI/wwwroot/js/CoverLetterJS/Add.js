9e8775e5984e5992434aee06eb3381139f5a6808
OsamaJa99 <osamajarrah111@gmail.com>
2025-06-21 07:23:21 +0300

    Edit Fornt

---
+++ ResumeAI/wwwroot/js/CoverLetterJS/Add.js
@@ -0,0 +1,41 @@
+﻿function buildStepsBreadcrumb(wizard, element, steps) {
+    const $steps = document.getElementById(element)
+    $steps.innerHTML = ''
+    for (let label in steps) {
+        if (steps.hasOwnProperty(label)) {
+            const $li = document.createElement('li')
+            const $a = document.createElement('a')
+            $li.classList.add('nav-item')
+            $a.classList.add('nav-link')
+            if (steps[label].active) {
+                $a.classList.add('active')
+            }
+            $a.setAttribute('href', '#')
+            $a.innerText = label
+            $a.addEventListener('click', e => {
+                e.preventDefault()
+                wizard.revealStep(label)
+            })
+            $li.appendChild($a)
+            $steps.appendChild($li)
+        }
+    }
+}
+
+function onStepChange(wizard, selector) {
+    const steps = wizard.getBreadcrumb()
+    buildStepsBreadcrumb(wizard, selector, steps)
+}
+
+const wizard = new window.Zangdar('#wizard', {
+    onStepChange: () => {
+        onStepChange(wizard, 'steps-native')
+    },
+    onSubmit(e) {
+        e.preventDefault()
+        console.log(e.target.elements)
+        return false
+    }
+})
+
+onStepChange(wizard, 'steps-native')
---
+++ ResumeAI/wwwroot/js/PortFolioJS/Add.js
@@ -0,0 +1,25 @@
+﻿    let serviceIndex = 1;
+    let projectIndex = 1;
+
+    $('#add-service').click(function () {
+            const html = `
+    <div class="service-item mb-3" data-index="${serviceIndex}">
+        <input name="Services[${serviceIndex}].ServiceName" class="form-control" placeholder="Service Name" />
+        <textarea name="Services[${serviceIndex}].ServiceDescription" class="form-control" placeholder="Service Description"></textarea>
+        <input type="file" name="Services[${serviceIndex}].ServiceImageFile" class="form-control" />
+    </div>`;
+    $('#services-container').append(html);
+    serviceIndex++;
+        });
+
+    $('#add-project').click(function () {
+            const html = `
+    <div class="project-item mb-3" data-index="${projectIndex}">
+        <input name="Projects[${projectIndex}].ProjectName" class="form-control" placeholder="Project Name" />
+        <textarea name="Projects[${projectIndex}].ProjectDescription" class="form-control" placeholder="Project Description"></textarea>
+        <input name="Projects[${projectIndex}].ProjectAttachments" class="form-control" placeholder="Project Attachment" />
+        <input name="Projects[${projectIndex}].ProjectLink" class="form-control" placeholder="Project Link" />
+    </div>`;
+    $('#projects-container').append(html);
+    projectIndex++;
+        });
